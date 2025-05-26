add1c7d1a6f45701b54fe8561a052d4856208091
Lawrence-km <148947765+Lawrence-km@users.noreply.github.com>
2025-05-26 20:45:11 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import ChallengeList from './components/ChallengeList';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Community Challenges</h1>
+      </header>
+      <main>
+        <ChallengeList />
+      </main>
+    </div>
+  );
+}
+
+export default App;
